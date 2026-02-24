7396331b76370e9d7ca44905fbbe017a3753a67a
antoine <antoine@delobbe.be>
2026-02-24 19:04:00 +0100

    Putting all classes that have private contructor in final class

--- src/main/java/states/stopwatch/ResetStopwatch.java
+++ src/main/java/states/stopwatch/ResetStopwatch.java
@@ -2,33 +2,32 @@ package states.stopwatch;
 
 import states.ClockState;
 
-public class ResetStopwatch extends AbstractStopwatch {
+public final class ResetStopwatch extends AbstractStopwatch {
 
 	// use Singleton design pattern
-    private ResetStopwatch() {};
-    private static ResetStopwatch instance = null;
-    public static ResetStopwatch Instance() {
-        if(instance == null) instance = new ResetStopwatch();
-        return instance;
-    }
+	private ResetStopwatch() {};
+	private static ResetStopwatch instance = null;
+	public static ResetStopwatch Instance() {
+		if(instance == null) instance = new ResetStopwatch();
+		return instance;
+	}
 
-    @Override
-    public ClockState up() {
-    	return transition(ActiveStopwatch.Instance());
-    	}
-	    
+	@Override
+	public ClockState up() {
+		return transition(ActiveStopwatch.Instance());
+	}
+
 	public String getUpText() { return "run"; }
-	    	    
+
 	@Override
 	protected void entry() {
 		super.entry();
 		totalTime = 0;
-	    lapTime = 0;
-	    }
+		lapTime = 0;
+	}
 
 	public String getDisplayString() {
 		return "totalTime = " + totalTime;
-	    }
-	     
 	}
 
+}
